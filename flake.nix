b8b1505f83ac7bd4dc550e121966556f507dde56
Tom Burdick <tom.burdick@electromtic.us>
2023-05-16 08:20:20 -0500

    Greatly simplify the devshell setup
    The custom derivations for pyocd and jlink are no longer needed.
    I don't need fvp or cmsis-pack-manager.
    
    nix develop works and I can build zephyr applications.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
-  description = "A Zephyr dev shell";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
-    flake-utils.url = "github:numtide/flake-utils/master";
-    devshell.url = "github:numtide/devshell/master";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
-  };
-  outputs = { self, nixpkgs, flake-utils, devshell }:
-    let
-      local-pythonpkgs = ".pythonpkgs";
-      mkZephyrShell = pkgs:
-        let 
-        zephyr-sdk = pkgs.zephyrSdk;
-        python-packages = pkgs.python3.withPackages (p: builtins.attrValues {
+  description = "virtual environments";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, devshell, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            config.segger-jlink.acceptLicense = true;
+            overlays = [ devshell.overlays.default ];
+          };
+          zephyr-sdk = pkgs.callPackage ./nix/zephyr-sdk.nix { };
+          python-packages = pkgs.python3.withPackages (p: builtins.attrValues {
             inherit (p)
               pyelftools
               pyyaml
@@ -39,54 +42,33 @@
               # check copliance deps
               junitparser
               magic;
-            # TODO: find a way to add this to the overlay
-            #imgtool = (p.callPackage ./nix/python-imgtool.nix { });
           });
         in
-        pkgs.devshell.mkShell {
-	  name = "zephyr-sdk-" + zephyr-sdk.version;
-          motd = "Zephyr Development with Nix\n" +
-		             " SDK: ${zephyr-sdk}";
-          packages = builtins.attrValues {
-            inherit
+          pkgs.devshell.mkShell {
+            name = "zephyr-sdk-" + zephyr-sdk.version;
+            motd = "Zephyr Development with Nix\n" +
+                   " SDK: ${zephyr-sdk}";
+            packages = [
               zephyr-sdk
               python-packages
-              ;
-            inherit (pkgs)
-              binutils
-              ninja
-              gperf
-              ccache
-              cmake
-              dtc
-              gnumake
-              # debug utilities
-              gdb;
-            inherit (pkgs.stdenv) cc;
-            inherit (pkgs.unixtools) xxd;
-            openssl-dev = pkgs.openssl.dev;
-            sqlite-dev = pkgs.sqlite.dev;
+              pkgs.pyocd
+              pkgs.binutils
+              pkgs.ninja
+              pkgs.gperf
+              pkgs.ccache
+              pkgs.cmake
+              pkgs.dtc
+              pkgs.gnumake
+              pkgs.gdb
+              pkgs.segger-jlink
+              pkgs.xxd
+              pkgs.nixpkgs-fmt
+            ];
+            env = [
+              { name = "ZEPHYR_SDK_INSTALL_DIR"; value = "${zephyr-sdk}"; }
+              { name = "PYTHONPATH"; eval = "${python-packages}/lib/python3.9/site-packages:$PYTHONPATH"; }
+              { name = "LD_LIBRARY_PATH"; eval = "${pkgs.libusb-compat-0_1}/lib:$LD_LIBRARY_PATH"; }
+            ];
           };
-          env = [
-            { name = "ZEPHYR_SDK_INSTALL_DIR"; value = "${zephyr-sdk}"; }
-            { name = "PYTHONPATH"; eval = "${python-packages}/lib/python3.9/site-packages:$PYTHONPATH"; }
-            # NOTE: I'm using the PIP_TARGET below to work around the fact
-            # that I can't install pyocd through nixpkgs at this time. Remove
-            # this when I find a way to install pyocd through nixpkgs.
-            { name = "PIP_TARGET"; value = local-pythonpkgs; }
-            { name = "LD_LIBRARY_PATH"; eval = "${pkgs.libusb-compat-0_1}/lib:$LD_LIBRARY_PATH"; }
-            { name = "ARMFVP_BIN_PATH"; value = pkgs.fvpCorestone; }
-          ];
-	};
-    in
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell = mkZephyrShell (import nixpkgs {
-        inherit system;
-        overlays = [
-          devshell.overlay
-          (import ./nix/overlay.nix)
-        ];
-      });
     });
 }
-
