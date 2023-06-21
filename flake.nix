611996f8681d76c19e5af90b29e15ec9f96d0fb2
Tom Burdick <thomas.burdick@intel.com>
2023-06-21 14:18:46 -0500

    Adds some environment variables for cavstwist
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
               devshell.overlays.default
             ];
           };
-          zephyr-sdk = pkgs.callPackage ./nix/zephyr-sdk.nix { version = "0.16.0"; };
+          zephyr-sdk = pkgs.callPackage ./nix/zephyr-sdk.nix { version = "0.16.1"; };
           rimage = pkgs.callPackage ./nix/rimage.nix { };
           python-packages = pkgs.python3.withPackages (p: builtins.attrValues {
             inherit (p)
@@ -88,10 +88,10 @@
               { name = "ZEPHYR_SDK_INSTALL_DIR"; value = "${zephyr-sdk}"; }
               { name = "PYTHONPATH"; eval = "${python-packages}/lib/python3.9/site-packages:$PYTHONPATH"; }
               { name = "LD_LIBRARY_PATH"; eval = "${pkgs.libusb-compat-0_1}/lib:$LD_LIBRARY_PATH"; }
-              { name = "ZEPHYR_BASE"; value = "$HOME/z/zephyr"; }
+              { name = "ZEPHYR_BASE"; value = "/home/tburdick/z/zephyr"; }
               { name = "CAVS_HOST"; value = "rawr"; }
-              { name = "CAVS_RIMAGE"; value = "$ZEPHYR_BASE/../modules/audio/sof/rimage"; }
-              { name = "CAVS_KEY"; value = "$ZEPHYR_BASE/../modules/audio/sof/keys/otc_private_key_3k.pem"; }
+              { name = "CAVS_RIMAGE"; eval = "$ZEPHYR_BASE/../modules/audio/sof/rimage"; }
+              { name = "CAVS_KEY"; eval = "$ZEPHYR_BASE/../modules/audio/sof/keys/otc_private_key_3k.pem"; }
               { name = "CAVS_OLD_FLASHER"; value = "1"; }
             ];
           };
