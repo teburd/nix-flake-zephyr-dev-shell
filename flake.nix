f0bf7059aba5f57f0de476b6aae9a853072d92ac
Tom Burdick <thomas.burdick@intel.com>
2023-10-13 10:55:09 -0500

    Lock in nix 23.05 for now so things don't break
    Drop usage of devshell, it seems something strange was being done
    with it such that nix develop -c zsh wouldn't work well. Using
    plain flakes with pkgs.mkShell seems to work just fine.

--- flake.nix
+++ flake.nix
@@ -1,73 +1,71 @@
 {
-  description = "virtual environments";
+  description = "Development Environment for Zephyr";
 
-  inputs.devshell.url = "github:numtide/devshell";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, flake-utils, devshell, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-            config.segger-jlink.acceptLicense = true;
-            overlays = [
-              devshell.overlays.default
-            ];
-          };
-          zephyr-sdk = pkgs.callPackage ./nix/zephyr-sdk.nix { version = "0.16.1"; };
-          rimage = pkgs.callPackage ./nix/rimage.nix { };
-          docleaf = ps: ps.callPackage ./nix/docleaf.nix { };
-          zephyr-python-packages = ps: with ps; [
-              pyelftools
-              pyyaml
-              pykwalify
-              canopen
-              packaging
-              progress
-              psutil
-              pylink-square
-              requests
-              anytree
-              west
-              cryptography
-              intelhex
-              click
-              cbor
-              jinja2
-              sigrok
-              pyocd
-              # imgtool -- mcuboot's imagetool
-              pip
-              pyusb
-              #twister deps
-              ply
-              pyserial
-              tabulate
-              GitPython
-              natsort
-              # doc deps
-              sphinx
-              sphinx-notfound-page
-              sphinx-copybutton
-              sphinx_rtd_theme
-              sphinx-tabs
-              sphinx-togglebutton
-              pygments
-              breathe
-              (docleaf ps)
-              # check copliance deps
-              junitparser
-              magic
-          ];
-          zephyr-python = pkgs.python3.withPackages zephyr-python-packages;
-        in
-          pkgs.devshell.mkShell {
-            name = "zephyr-" + zephyr-sdk.version;
-            motd = "Zephyr Development with Nix\n" +
-                   " SDK: ${zephyr-sdk}";
-            packages = [
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.segger-jlink.acceptLicense = true;
+        };
+        zephyr-sdk = pkgs.callPackage ./nix/zephyr-sdk.nix { version = "0.16.1"; };
+        rimage = pkgs.callPackage ./nix/rimage.nix { };
+        docleaf = ps: ps.callPackage ./nix/docleaf.nix { };
+        zephyr-python-packages = ps: with ps; [
+          pyelftools
+          pyyaml
+          pykwalify
+          canopen
+          packaging
+          progress
+          psutil
+          pylink-square
+          requests
+          anytree
+          west
+          cryptography
+          intelhex
+          click
+          cbor
+          jinja2
+          sigrok
+          pyocd
+          # imgtool -- mcuboot's imagetool
+          pyusb
+          #twister deps
+          ply
+          pyserial
+          tabulate
+          GitPython
+          natsort
+          # doc deps
+          sphinx
+          sphinx-notfound-page
+          sphinx-copybutton
+          sphinx_rtd_theme
+          sphinx-tabs
+          sphinx-togglebutton
+          pygments
+          breathe
+          (docleaf ps)
+          # check copliance deps
+          junitparser
+          magic
+          # protobuf modules
+          protobuf
+          grpcio-tools
+        ];
+        zephyr-python = pkgs.python3.withPackages zephyr-python-packages;
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
               zephyr-sdk
               rimage
               zephyr-python 
@@ -86,15 +84,17 @@
               pkgs.pkgsi686Linux.gcc
               pkgs.doxygen
               pkgs.graphviz-nox
+              pkgs.protobuf
             ];
-            env = [
-              { name = "ZEPHYR_SDK_INSTALL_DIR"; value = "${zephyr-sdk}"; }
-              { name = "ZEPHYR_BASE"; value = "/home/tburdick/z/zephyr"; }
-              { name = "CAVS_HOST"; value = "rawr"; }
-              { name = "CAVS_RIMAGE"; eval = "$ZEPHYR_BASE/../modules/audio/sof/rimage"; }
-              { name = "CAVS_KEY"; eval = "$ZEPHYR_BASE/../modules/audio/sof/keys/otc_private_key_3k.pem"; }
-              { name = "CAVS_OLD_FLASHER"; value = "1"; }
-            ];
+            shellHook =''
+              export ZEPHYR_SDK_INSTALL_DIR="${zephyr-sdk}"
+              export ZEPHYR_BASE=/home/tburdick/z/zephyr
+              export CAVS_HOST="rawr"
+              export CAVS_RIMAGE="$ZEPHYR_BASE/../modules/audio/sof/rimage"
+              export CAVS_KEY="$ZEPHYR_BASE/../modules/audio/sof/keys/otc_private_key_3k.pem"
+              export CAVS_OLD_FLASHER=1
+              '';
           };
-    });
+        }
+    );
 }
